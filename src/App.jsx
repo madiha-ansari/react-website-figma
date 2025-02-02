6f7c427657126de9614031f7f36d6b1d2048ff12
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-02-02 09:15:49 +0500

    testimonails section completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Experiences from './components/Experiences'
 import About from './components/About'
 import Projects from './components/Projects'
+import Customer from './components/Customer'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Experiences></Experiences>
       <About></About>
 <Projects></Projects>
+<Customer></Customer>
 
     </div>
   )
---
+++ src/components/Customer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import customer from "../assets/Testimonial.png";  // Image import
+
+const Customer = () => {
+    return (
+        <div><div>
+            <img
+                src={customer}
+                alt="Navbar"
+                style={{ width: '1480px', height: '696px' }}
+            />
+        </div>
+
+
+        </div>
+    )
+}
+
+export default Customer
