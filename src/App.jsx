e77acb69a95a2b0e171706e822a3d0f7458326c8
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-02-02 08:58:05 +0500

    Experience section has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Header from './components/Header'
+import Experiences from './components/Experiences'
 
 const App = () => {
   return (
     <div>
       <NavBar></NavBar>
       <Header></Header>
+      <Experiences></Experiences>
 
     </div>
   )
--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import skills from "../assets/My Skill.png";  // Image import
+
 
 const Experiences = () => {
   return (
-    <div>
+    <div><div>
+            <img
+                              src={skills}
+                              alt="Navbar"
+                              style={{ width: '1480px', height: '506px' }}
+                          />
+    </div>
       
     </div>
   )
